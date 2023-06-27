74adb79c2761a32ba9d6f61fb123c5d91a7fcae9
laxman3452 <137712758+laxman3452@users.noreply.github.com>
2023-06-27 17:22:11 +0545

    intro to basic python

--- intro-to-python.py
+++ intro-to-python.py
@@ -1,3 +1,23 @@
-changed again
+print('Hello, World')
 
-nice
+# memory reference id()
+a = 6
+print(id(a))
+
+# adding string
+print("hello" + " world")
+
+# type to get what data type
+str = "hi"
+print(type(str))
+print(str[1])
+# to print last letter of string
+print(str[-1])
+
+str2 = "hello,world"
+print(str2[2:5])
+# step size
+print(str2[2:5:2])
+print(str2[2::2])
+print(str2[:6])
+print(str2[6:])
