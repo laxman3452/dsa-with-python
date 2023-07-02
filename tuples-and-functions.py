d3e68da53743a2c500c8d432890ea71c3ee7e7b5
laxman3452 <137712758+laxman3452@users.noreply.github.com>
2023-07-02 07:58:11 +0545

    tuples and functions in python

---
+++ tuples-and-functions.py
@@ -0,0 +1,26 @@
+a = [1,2,3,4,5]
+
+b = [x**4 for x in a if x%2==0]
+print(b)
+
+c = [x for x in range(1,10)]
+
+print(c)
+
+# loop -for(faster and for known number of iteration.),while(conditon based no known number of iteration)
+
+# module is another file to be included in another file.
+# import sys
+
+
+# Tuples
+# (1,2,3,4,5)
+# tuples are immutable but it's element is changed how because it's member are referenced and actual data iss not present inside it. it contains data references.
+
+# tuples size do not increase even if members are increased.
+
+
+
+# function definition (x,y) --parameters
+# function call (4,5) --arguments
+
